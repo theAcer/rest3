fac7b0b8d53a7d17065aff3259993eaae9021928
Developer <Developer@DESKTOP-URBC0PG>
2018-06-20 13:10:01 +0300

    rest swagger api calculator

---
+++ test/test/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        // GET: api/Calculator
+        public IEnumerable<Calculator> Get()
+        {
+            var calculatorList = new List<Calculator>();
+            for (int i = 0; i < 11; i++)
+            {
+               var Calculator = new Calculator
+                {
+                    Location = $"Location {i}",
+                    Degree = i * 20 / 15,
+                    DateTime = DateTime.Now.ToUniversalTime()
+                };
+                calculatorList.Add(Calculator);
+             
+            }
+            return calculatorList;
+        }
+
+        // GET: api/Calculator/5
+        public Calculator Get(int id)
+        {
+            return new Calculator
+            {
+                Location = $"Location {id}",
+                Degree = id * 20 / 15,
+                DateTime = DateTime.Now.ToUniversalTime()
+            };
+        }
+
+
+    }
+}
---
+++ test/test/Models/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+
+namespace test.Models
+{
+    [DataContract]
+
+    public class Calculator
+    {
+        [DataMember(Name = "location")]
+        public string Location { get; set; }
+
+        [DataMember(Name = "degree")]
+        public float Degree { get; set; }
+
+        [DataMember(Name = "datetime")]
+        public DateTime DateTime { get; set; }
+    }
+}
